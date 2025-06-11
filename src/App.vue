1827cc46049d6dd04379a050bfe352188fdb853d
Akram Amkeched Ouriaghli <akram.amk00@gmail.com>
2025-06-11 16:19:20 +0200

    code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,55 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+
+const header = ref('Shopping List App')
+const reversedItems = computed(()=>[...items.value].reverse())
+const editing = ref(false)
+const items = ref([
+  { label: '10 party hats', purchased: true, highPriority: false, id: 1 },
+  { label: '2 board games', purchased: true, highPriority: false, id: 2 },
+  { label: '20 cups', purchased: false, highPriority: true, id: 3 }
+])
+const newItem = ref('')
+const newItemHighPriority = ref(false)
+const iceCreamFlavors = ref([])
+const addItem = () => {
+  items.value.push({ label: newItem.value, purchased: false, id: items.value.length + 1, highPriority: newItemHighPriority.value})
+  newItem.value = ''
+  newItemHighPriority.value = false;
+}
+const doEdit = (e) => {
+  editing.value = e
+  newItem.value = ''
+}
+const togglePurchased = (item) => {
+  item.purchased = ! item.purchased
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" class="btn" @click="doEdit(false)">
+      Cancel
+    </button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">
+      Add Item
+    </button>
+  </div>
+  <form class="add-item-form" @submit.prevent="addItem" v-if="editing">
+    <input v-model.trim="newItem" type="text" placeholder="Add an item">
+    <label>
+      <input v-model="newItemHighPriority" type="checkbox">
+      High Priority
+    </label>
+    <button class="btn btn-primary" :disabled="!newItem.length">
+      Add Item
+    </button>
+  </form>
+  <ul>
+    <li @click="togglePurchased(item)" v-for="(item, index) in reversedItems" key="item.id" :class="{strikeout: item.purchased, priority: item.highPriority}">
+      {{ item.label }}
+    </li>
+  </ul>
+  <p v-if="!items.length">Nothing to see here</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
